85dd8735660a64fc21f0b91105cde2a4be8cdd46
elijahred23 <elijahred23@gmail.com>
2023-12-26 22:17:58 -0600

    Eli-4: Fixed syntax errors

--- src/farkleLogic/PlayerDice.js
+++ src/farkleLogic/PlayerDice.js
@@ -7,10 +7,15 @@ const PlayerDice = {
     randomDie() {
         return Math.floor(Math.random() * 6) + 1;
     },
-    roll() {
+    roll(dice = null) {
         for (let i = 0; i < 6; i++) {
-            this.dice[i] = this.randomDie();
+            if(dice === null){
+                this.dice[i] = this.randomDie();
+            } else {
+                dice[i] = this.randomDie();
+            }
         }
+        if(dice !== null) return dice;
     },
     replaceDice(dice) {
         for (let i = 0; i < dice.length; i++) {
@@ -19,6 +24,7 @@ const PlayerDice = {
         return dice;
     },
     oneOrFive: 0,
+    straight: 0,
     hasThreeOfNum: {
         1: 0,
         2: 0,
@@ -30,16 +36,28 @@ const PlayerDice = {
     totalCounts: 2000,
     calculateProbability(method, number = null){
         const { keptDice, availableDice } = this.splitKeptAvailabeDice();
-        let tempDice = this.replaceDice(availableDice);
+        let tempDice = this.roll(availableDice);
         let counter = 0;
         for (let i = 0; i < this.totalCounts; i++) {
             let hasIt = number !== null ? method(number, tempDice) : method(tempDice);
             if (hasIt) {
                 counter++;
             }
-            tempDice = this.replaceDice(availableDice);
+            tempDice = this.roll(availableDice);
         }
-        this.hasThreeOfNum[number] = counter / this.totalCounts;
+        let probability = counter / this.totalCounts;
+        if(number !== null){
+            this.hasThreeOfNum[number] = probability;
+        } else {
+            switch(method.name){
+                case "hasOneOrFive": this.oneOrFive = probability; 
+                    break;
+                case "hasStraight": this.straight = probability;
+                    break;
+            }
+        }
+        return probability;
+        
     },
     hasThree(number, dice = null) {
         const countOfNumber = dice === null ? this.dice.filter(element => element === number).length :
@@ -47,18 +65,43 @@ const PlayerDice = {
         return countOfNumber >= 3;
     },
     splitKeptAvailabeDice() {
-        keptDice = this.dice.filter((_, index) => this.keptDice.includes(index));
-        availableDice = this.dice.filter((_, index) => !this.keptDice.includes(index));
+        let keptDice = this.dice.filter((_, index) => this.keptDice.includes(index));
+        let availableDice = this.dice.filter((_, index) => !this.keptDice.includes(index));
         return { keptDice, availableDice };
     },
     hasOneOrFive(dice = null) {
         return dice === null ? (this.dice.includes(1) || this.dice.includes(5)) :
             (dice.includes(1) || dice.includes(5))
     },
+    hasStraight(dice = null){
+        if( dice !== null && dice.length < 6){
+            return false;
+        } else if (dice === null && this.dice.length < 6){
+            return false;
+        }
+        
+        let straightNumbers =  [1,2,3,4,5,6];
+        let isStraight = dice === null ? straightNumbers.every(num=>this.dice.includes(num)) : straightNumbers.every(num=>dice.includes(num));
+        return isStraight; 
+    },
     calculate(){
         for (let number = 1; number <= 6; number++) {
             this.calculateProbability(this.hasThree, number);
         }
+        this.calculateProbability(this.hasOneOrFive);
+        this.calculateProbability(this.hasStraight);
+
+    },
+    decimalToPercent(decimal){
+        return (decimal * 100).toFixed(2) + '%';
+    },
+    printCalculations(){
+        console.log('\n***********************************\nProbabilities')
+        for(let number = 1; number <= 6; number++){
+            console.log(`Three ${number}s ${this.decimalToPercent(this.hasThreeOfNum[number])}`);
+        }
+        console.log(`Will get one or five: ${this.decimalToPercent(this.oneOrFive)}`)
+        console.log(`Will get straight: ${this.decimalToPercent(this.straight)}`)
     }
 }
 PlayerDice.init();
--- src/farkleLogic/Test.js
+++ src/farkleLogic/Test.js
@@ -6,6 +6,4 @@ PlayerDice.init();
 console.log(PlayerDice.dice);
 PlayerDice.calculate();
 
-for(let number = 1; number <= 6; number++){
-    console.log(`Three ${number}s ${PlayerDice.hasThreeOfNum[number]}`);
-}
+PlayerDice.printCalculations();
